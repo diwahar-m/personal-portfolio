3e36c47fc3ae8a730806964106118a31d16cf41a
diwahar-m <msdiwahar5@gmail.com>
2025-05-07 23:57:34 +0530

    lucide added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import { Divider } from "@mui/material";
+import { ArrowRight } from "lucide-react";
 import React from "react";
 
 export default function Projects() {
@@ -11,8 +12,9 @@ export default function Projects() {
           </h3>
           <Divider sx={{ backgroundColor: "#C778DD", width: "35rem" }} />
         </div>
-        <div>
-          <p className="text-white">View all ➡️</p>
+        <div className="flex items-center gap-1">
+          <p className="text-white">View all </p>
+          <ArrowRight size={"20"} color="#fff" />
         </div>
       </div>
     </div>
